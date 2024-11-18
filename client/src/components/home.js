7cd58a38cec4c10b9a985d868f3447b59b5c401a
Caique Menezes <caiquemenezes1@outlook.com>
2024-11-18 16:09:38 -0300

    ✨ feat: user component form, user styles

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -5,39 +5,36 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
 import '../styles/home.style.css';
 
-
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <div className='home-container'>
-
             <div className='home-container-box'>
-
                 <TextField
-                        required
-                        id="outlined-required"
-                        label="Mail"
-                        type='mail'
-                        defaultValue="Hello World"
-                        />
-                        <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        />
-                        <div className='btn-container'>
-                            <Button color="secondary" variant='contained' id='btn-login'>Entrar</Button>
-                        </div>
-                        <div className='sing-in-container'>
-                            <p className='sing-up-text'>Sing-up</p>
-                            <div className='icons-container'>
-                            <InstagramIcon id='icons-instagram'/>
-                            <XIcon id='icons-x'/>
-                            </div>
-                        </div>
+                    required
+                    id="outlined-required"
+                    label="Mail"
+                    type='mail'
+                    defaultValue="Hello World"
+                />
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                />
+                <div className='btn-container'>
+                    <Button  variant='contained' id='btn-login'>Entrar</Button>
+                </div>
+                <div className='sing-in-container'>
+                    <p className='sing-up-text'>Sign-up</p>
+                    <div className='icons-container'>
+                        <InstagramIcon id='icons-instagram' />
+                        <XIcon id='icons-x' />
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
